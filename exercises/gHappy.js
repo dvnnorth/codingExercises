28ac0c616320b732cfe5ef9619005e3fb285cf3f
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2019-04-27 21:31:34 -0500

    Completed gHappy.js, passed tests

--- exercises/gHappy.js
+++ exercises/gHappy.js
@@ -1,12 +1,29 @@
 module.exports.run = function(str) {
-/*
-A lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right. Return true if all the g's in the given string are happy, otherwise return false.
+  /*
+		A lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right. Return true if all the g's in the given string are happy, otherwise return false.
 
-	Example:
-		function("xxggxx") => true
-		function("xxgxx") => false
+		Example:
+			function("xxggxx") => true
+			function("xxgxx") => false
 
-        Write your code below the comment.
-*/
-
+		Write your code below the comment.
+	*/
+	// Little helper function for clean-up. Just checks equivalency to str 'g'
+	let isG = letter => letter === 'g';
+	
+	// Return reduction of str. Reduce to disprove isHappy
+	// Simple for loop is slightly fewer operations but like it functional, YOLO
+  return str.split('').reduce((isHappy, letter, i, letters) => {
+		// If isHappy has already been disproven, no need to check again
+    if (isG(letter) && isHappy) {
+      if (i === 0) {
+        isHappy = isG(letters[i + 1]);
+      } else if (i > 0 && i < letters - 1) {
+        isHappy = isG(letters[i - 1]) || isG(letters[i + 1]);
+      } else {
+        isHappy = isG(letters[i - 1]);
+      }
+    }
+    return isHappy;
+  }, true);
 };
